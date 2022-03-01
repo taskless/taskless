15153aa24ad42d2a0c3ea8464a0f934bdece3ad0
Jakob Heuser <EMAIL>
2022-03-01 15:09:36 -0800

    refactor: Adds support for prettier, lint, and husky

---
+++ .lintstagedrc.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+const common = ["prettier --write"];
+
+module.exports = {
+  // global configurations, these are always cleaned
+  "*.{json,gql,graphql,md,yaml,yml}": [...common],
+
+  // next.js
+  [`{examples/next/*,examples/next/**/*}.{js,ts,jsx,tsx,vue}`]: [
+    ...common,
+    // https://nextjs.org/docs/basic-features/eslint#lint-staged
+    ...[
+      (filenames) =>
+        `yarn workspace taskless-example-next lint --fix --file ${filenames
+          .map((f) => path.relative(process.cwd(), f))
+          .join(" --file ")}`,
+    ],
+  ],
+
+  // taskless
+  [`{packages/taskless/*, packages/taskless/**/*}.{js,ts,jsx,tsx,vue}`]: [
+    ...common,
+    "yarn workspace taskless lint --fix",
+  ],
+};
