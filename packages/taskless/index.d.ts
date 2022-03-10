4efc46dffb9560fb28761b9f680d862dab05e9b1
Jakob Heuser <jakob@codedrift.com>
2022-03-10 13:03:15 -0800

    fix: Fixes type exports for main and next

---
+++ packages/taskless/index.d.ts
@@ -0,0 +1,3 @@
+// expose types from tsc
+export * from "./dist/esm/index";
+export * from "./dist/esm/types";
---
+++ packages/taskless/next.d.ts
@@ -0,0 +1,3 @@
+// expose types from tsc
+export * from "./dist/esm/next";
+export * from "./dist/esm/types";
